737e2def49c8e21327ee88a4b8f64a4effd1cbf0
wokeisha <oleggladilovwork@gmail.com>
2021-03-21 16:37:45 +0500

    webpack init

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    babelOptions: {
+      configFile: './babel.config.json'
+    }
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  extends: ['eslint:recommended', 'google'],
+  rules: {
+    'semi': 'off',
+    'linebreak-style': 0,
+    'indent': 'off',
+    'comma-dangle': 'off',
+    'require-jsdoc': 'off',
+    'eol-last': 0,
+    'spaced-comment': 'off',
+    'arrow-parens': 'off',
+    'operator-linebreak': 'off',
+    'max-len': 'off',
+    'prefer-const': 'off'
+  }
+};
