47d3aa3c04d00604ea46108ad25206401ef27fab
wokeisha <oleggladilovwork@gmail.com>
2021-03-27 02:33:39 +0500

    finish base table logic

--- src/core/dom.js
+++ src/core/dom.js
@@ -11,6 +11,16 @@ class Dom {
     }
     return this.$el.outerHTML.trim()
   }
+  text(text) {
+    if (typeof text === 'string') {
+      this.$el.textContent = text
+      return this
+    }
+    if (this.$el.tagName.toLowerCase() === 'input') {
+      return this.$el.value.trim()
+    }
+    return this.$el.textContent.trim()
+  }
   clear() {
     this.html('')
     return this
@@ -41,8 +51,31 @@ class Dom {
   get data() {
     return this.$el.dataset
   }
+  addClass(className) {
+    this.$el.classList.add(className)
+  }
+  removeClass(className) {
+    this.$el.classList.remove(className)
+  }
+  find(selector) {
+    return $(this.$el.querySelector(selector))
+  }
   findAll(selector) {
     return this.$el.querySelectorAll(selector)
+  }
+  id(parse) {
+    if (parse) {
+      const parsed = this.id().split(':')
+      return {
+        row: +parsed[0],
+        col: +parsed[1]
+      }
+    }
+    return this.data.id
+  }
+  focus() {
+    this.$el.focus()
+    return this
   }
   css(styles ={}) {
     Object
